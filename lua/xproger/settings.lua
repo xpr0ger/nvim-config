af9dc8145ca6708056bf5c59ca4905b7b2afdb07
Denys Mereshchenko <xproger.ru@gmail.com>
2023-10-06 18:49:55 +0300

    added listchars to display unprintable characters

--- lua/xproger/settings.lua
+++ lua/xproger/settings.lua
@@ -5,3 +5,12 @@ vim.o.shiftwidth = 4
 vim.o.termguicolors = true
 vim.wo.relativenumber = true
 vim.o.cursorline = true
+vim.opt.listchars = {space = "·",
+    tab = "╴╴󰌒",
+    eol = "󰌑",
+    nbsp = "␣",
+    trail = "•",
+    extends = "⟩",
+    precedes = "⟨"}
+vim.opt.list = true
+vim.opt.showbreak = "↪"
